66c4a49f03776dc0438d01a718f7b2eca32fe9a2
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2024-09-22 20:05:05 -0300

    add react root files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Content from "./components/Content/Content";
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Content />
+    </div>
+  );
+}
+
+export default App;
