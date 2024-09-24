455506fda1511b9ce7154314f5e4f1bc12947551
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2024-09-24 20:02:02 -0300

    add photo get

--- src/components/Photos/Photos.js
+++ src/components/Photos/Photos.js
@@ -1,17 +1,22 @@
 import React from "react";
 import styles from "./Photos.module.css";
+import PhotosContent from "./PhotosContent";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchPhotos } from "../../redux/stores/photos/photos";
 
 const Photos = () => {
   const dispatch = useDispatch();
-  // const { photos } = useSelector((state) => state.login.token)
+  const { data } = useSelector((state) => state.photos);
 
   React.useEffect(() => {
     dispatch(fetchPhotos(1));
   }, [dispatch]);
 
-  return <section className={styles.container}>Photos</section>;
+  return (
+    <section className={styles.container}>
+      {data ? <PhotosContent /> : <span>Sem fotos</span>}
+    </section>
+  );
 };
 
 export default Photos;
