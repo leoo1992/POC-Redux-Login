9c77926340679a8780547dba1ecb80f5aee18808
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2024-09-24 22:33:50 -0300

    adjust render logic

--- src/components/Photos/Photos.js
+++ src/components/Photos/Photos.js
@@ -5,25 +5,44 @@ import { useSelector, useDispatch } from "react-redux";
 import { loadNewPhotos } from "../../redux/stores/photos/photos";
 import AddPhotosBtn from "../UX/Buttons/AddPhotosBtn/AddPhotosBtn";
 import ResetPhotosBtn from "../UX/Buttons/ResetPhotosBtn/ResetPhotosBtn";
+import Loader from "../UX/Loader/Loader";
 
 const Photos = () => {
   const dispatch = useDispatch();
-  const { data, infinite } = useSelector((state) => state.photos);
+  const { data, infinite, loading } = useSelector((state) => state.photos);
 
   React.useEffect(() => {
     dispatch(loadNewPhotos(1));
   }, [dispatch]);
 
+  const renderContent = () => {
+    if (data) {
+      return <PhotosContent />;
+    }
+    return <span>Sem fotos</span>;
+  };
+
+  const renderFooter = () => {
+    if (!infinite && !loading) {
+      return (
+        <div className={styles.endPhotos}>
+          <span>Sem mais fotos</span>
+          <ResetPhotosBtn text="Reset" />
+        </div>
+      );
+    }
+
+    if (loading) {
+      return <Loader />;
+    }
+
+    return <AddPhotosBtn text="+" />;
+  };
+
   return (
     <section className={styles.container}>
-      {data ? <PhotosContent /> : <span>Sem fotos</span>}
-      {infinite ? (
-        <AddPhotosBtn text="+" />
-      ) : (
-        <div className={styles.endPhotos}>
-          <span>Sem mais fotos</span> <ResetPhotosBtn text="Reset" />
-        </div>
-      )}
+      {renderContent()}
+      {renderFooter()}
     </section>
   );
 };
